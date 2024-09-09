aff8de31297fcb99a7e393a825f3963bc3839477
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-09-09 17:57:28 -0500

    Update text_utils.py

--- text_utils.py
+++ text_utils.py
@@ -3,6 +3,6 @@
         print(x)
 
     def capitalize_string(s):
-        x = input("Input a string of words")
-        return x.upper(x)
+        s = input("Input a string of words")
+        return s.upper(s)
        
