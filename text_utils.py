d61815523200573a304081bf8c3e80d66f77667b
Reggie Joseph <64855625+rjsmart021@users.noreply.github.com>
2024-09-09 17:51:14 -0500

    Create text_utils.py

---
+++ text_utils.py
@@ -0,0 +1,8 @@
+    def reverse_string(x):
+        return x[::-1]
+        print(x)
+
+    def capitalize_string(s):
+        x = input("Input a string of words")
+        return x.upper(x)
+       
